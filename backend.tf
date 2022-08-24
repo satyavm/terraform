acf289e9c73d24e5b575cbffa9aac890ea85cbcf
Satya Saranu <Satya.Saranu@fluenceenergy.com>
2022-08-23 20:26:18 -0500

    updated pipeline

--- backend.tf
+++ backend.tf
@@ -3,6 +3,5 @@ terraform {
     bucket = "terraform-state-test-0001"
     key    = "ec2/terraform.tfstate"
     region = "us-east-1"
-    #     dynamodb_table = "dynamodb-terraform-state-lock"
-  }
+}
 }
